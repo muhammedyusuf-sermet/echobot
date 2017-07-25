72ca25d7011625aa6b3ba8f8b0fca52183673753
muhammedyusuf_sermet <muhammedyusuf-sermet@uiowa.edu>
2017-07-25 22:46:59 +0000

    Port updated

--- server.js
+++ server.js
@@ -219,6 +219,6 @@ server.get(/.*/, restify.serveStatic({
 	'default': 'index.html'
 }));
 
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port || 3000, function () { //3978
     console.log('%s listening to %s', server.name, server.url); 
 });
